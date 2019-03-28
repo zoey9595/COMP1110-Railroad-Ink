fff92c58abd65730be1b35689380643b85af58bb
Yuqing <Zhai(u6865190@anu.edu.au)>
2019-03-28 15:26:06 +1100

    Finished lab4.1

---
+++ src/comp1110/ass2/gittest/A.java
@@ -0,0 +1,9 @@
+package comp1110.ass2.gittest;
+
+public class A {
+   @Override
+    public String toString(){
+       return null;
+   }
+
+}
---
+++ src/comp1110/ass2/gittest/Main.java
@@ -0,0 +1,4 @@
+package comp1110.ass2.gittest;
+
+public class Main {
+}
